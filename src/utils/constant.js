6a15efd2801c7c3d7a5a5fd083628faf4ac575df
santoshsharma27 <santosh37kr@gmail.com>
2024-10-26 22:04:46 +0530

    fixed constant file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,11 +1,9 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-export const SWIGGY_API =
-  "https://swiggybackendserver.onrender.com/api/restaurants?lat=12.9046136&lng=77.614948&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_API = `${import.meta.env.VITE_SWIGGY_APP_BASE_URL}restaurants?lat=12.9046136&lng=77.614948&page_type=DESKTOP_WEB_LISTING`;
 
-export const MENU_API =
-  "https://swiggybackendserver.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&&submitAction=ENTER&restaurantId=";
+export const MENU_API = `${import.meta.env.VITE_SWIGGY_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&&submitAction=ENTER&restaurantId=`;
 
 export const USERNAME = "Santosh";
 
@@ -16,11 +14,3 @@ export const Linkedin_URL = "https://www.linkedin.com/in/santoshsharma37/";
 
 // export const MENU_API =
 //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId=";
-
-// https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId
-
-// export const SWIGGY_API =
-//   "https://foodfire.onrender.com/api/restaurants?lat=12.9046136&lng=77.614948&page_type=DESKTOP_WEB_LISTING";
-
-// export const MENU_API =
-//   "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&&submitAction=ENTER&restaurantId=";
